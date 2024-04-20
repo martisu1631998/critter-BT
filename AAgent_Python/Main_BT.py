a772ccda757b87c807f623898d973c8b29684b40
Arnau <1630311@uab.cat>
2024-04-20 13:14:53 +0200

    ordenant codi

--- AAgent_Python/Main_BT.py
+++ AAgent_Python/Main_BT.py
@@ -1,14 +1,9 @@
 import asyncio
-import random
 import time
-import py_trees as pt  
-from py_trees import common
+import py_trees as pt 
 from Conditions_BT import *
-from AAgent_Python.Actions_BT import *
+from Actions_BT import *
 from Goals_BT import *
-import Sensors
-from AAgent_Python.Actions_BT import *
-
 
 '''
 Main behaviour tree that models the movements of our agent, the critter.
@@ -21,52 +16,45 @@ class GlobalBT:
         self.aagent = aagent
         self.aagent.i_state.initTime = time.time()               
         
-        # Create sub-trees
-         # Avoid critters
-        critter = pt.composites.Sequence(name="Avoid critter", memory=True)
+        ''' Create subtrees '''
+        # Avoid critters
+        critter = pt.composites.Sequence(name="Avoid critters", memory=True)
         critter.add_children([Is_Critter(aagent), 
                               BN_Turn(aagent, -1, 45), 
                               BN_Turn(aagent, 1, 90), 
                               BN_Forward(aagent)])
 
         # Eat flowers
-        eat = pt.composites.Sequence("Eat", memory=True)
+        eat = pt.composites.Sequence("Eat flowers", memory=True)
         eat.add_children([Is_Hungry(aagent), Is_Flower(aagent), TurnToFlower(aagent), GoToFlower(aagent), Eating(aagent)])
  
         # Avoid obstacles
         front = pt.composites.Sequence("Avoid front", memory=True)
-        front.add_children([Is_My_Obstacle(aagent, 5, 6), BN_Turn(aagent, 1)])
+        front.add_children([Is_Obstacle(aagent, 5, 6), BN_Turn(aagent, 1)])
 
         right = pt.composites.Sequence("Avoid right", memory=True)
-        right.add_children([Is_My_Obstacle(aagent, 6, 7), BN_Turn(aagent, -1)])
+        right.add_children([Is_Obstacle(aagent, 6, 7), BN_Turn(aagent, -1)])
 
         left = pt.composites.Sequence("Avoid left", memory=True)
-        left.add_children([Is_My_Obstacle(aagent, 4, 5), BN_Turn(aagent, 1)])
+        left.add_children([Is_Obstacle(aagent, 4, 5), BN_Turn(aagent, 1)])
 
         Avoid_Obs = pt.composites.Selector("Avoiding", memory=False)
         Avoid_Obs.add_children([left, front, right])
 
         obstacle = pt.composites.Sequence("Avoid obstacles", memory=True)
-        obstacle.add_children([Is_My_Obstacle(aagent, 4, 7), Avoid_Obs])
-        # obstacle = pt.composites.Sequence("Avoid obstacles", memory=True)
-        # obstacle.add_children([Is_Obstacle(aagent), ManageObs(aagent)])
+        obstacle.add_children([Is_Obstacle(aagent, 4, 7), Avoid_Obs])
 
         # Follow astronaut
         follow = pt.composites.Sequence("Follow astronaut", memory=True)
         follow.add_children([Is_Astronaut(aagent), TurnToAstronaut(aagent), GoToAstronaut(aagent)])
 
-        # # Search astronaut
-        # search = pt.composites.Sequence("Search astronaut", memory=True)
-        # search.add_children([Is_Following(aagent), Searching(aagent)])
-
         # Roam around
-        roaming = pt.composites.Parallel("Parallel", policy=pt.common.ParallelPolicy.SuccessOnAll())
+        roaming = pt.composites.Parallel("Roam around", policy=pt.common.ParallelPolicy.SuccessOnAll())
         roaming.add_children([BN_Forward(aagent), BN_Turn(aagent)])
 
         # Tree root selector
-        self.root = pt.composites.Selector(name="Selector", memory=False)
-        self.root.add_children([critter, eat, obstacle, follow, roaming]) # search(aagent)
-
+        self.root = pt.composites.Selector(name="Main", memory=False)
+        self.root.add_children([critter, eat, obstacle, follow, roaming])
         self.behaviour_tree = pt.trees.BehaviourTree(self.root)
 
     # Function to set invalid state for a node and its children recursively
@@ -82,8 +70,3 @@ class GlobalBT:
     async def tick(self):
         self.behaviour_tree.tick()
         await asyncio.sleep(0)
-
-
-
-# xd = GlobalBT(aagent={})
-
