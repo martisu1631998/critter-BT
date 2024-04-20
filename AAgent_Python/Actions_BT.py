600a0bb7d3b6ef6ec604936d8c860343cc8b6dd4
Arnau <1630311@uab.cat>
2024-04-20 12:45:54 +0200

    ordenant codi

--- AAgent_Python/Actions_BT.py
+++ AAgent_Python/Actions_BT.py
@@ -7,85 +7,87 @@ from Base_Actions import *
 
 '''
 All the main behaviour tree's action leaves.
-They represent different actions that the agent can perform.
+They represent the different activities, more or less complex, that the agent can perform.
 '''
 
-class ManageObs(pt.behaviour.Behaviour):
-    """
-    Manage Obstacle:
-    This behaviour tree (together witth IsObstacle) only modifies the behaviour of the 
-    critter when it has an obstacleright in front. When it has an obstacle close on the 
-    right or in the left, it walks forward but it remembers where the obstacle is, in order
-    to turn the opposite side when it cannot advance.
-    """
-    def __init__(self, aagent):
-        self.my_goal = None
-        print("Initializing ManageObs")
-        super(ManageObs, self).__init__("ManageObs")
-        self.logger.debug("Initializing ManageObs")
-        self.my_agent = aagent
+# class ManageObs(pt.behaviour.Behaviour):
+#     """
+#     Manage Obstacle:
+#     This behaviour tree (together witth IsObstacle) only modifies the behaviour of the 
+#     critter when it has an obstacleright in front. When it has an obstacle close on the 
+#     right or in the left, it walks forward but it remembers where the obstacle is, in order
+#     to turn the opposite side when it cannot advance.
+#     """
+#     def __init__(self, aagent):
+#         self.my_goal = None
+#         print("Initializing ManageObs")
+#         super(ManageObs, self).__init__("ManageObs")
+#         self.logger.debug("Initializing ManageObs")
+#         self.my_agent = aagent
 
-    def initialise(self):
-        # No immediate obstacle --> do nothing
-        if self.my_agent.i_state.obstacleInfo[1] == 0:
-            print("Ignore Obstacle", self.my_agent.i_state.obstacleInfo)
-            self.my_goal = asyncio.create_task(Goals_BT.ForwardDist(self.my_agent, -1, 1, 5).run())
+#     def initialise(self):
+#         # No immediate obstacle --> do nothing
+#         if self.my_agent.i_state.obstacleInfo[1] == 0:
+#             print("Ignore Obstacle", self.my_agent.i_state.obstacleInfo)
+#             self.my_goal = asyncio.create_task(Goals_BT.ForwardDist(self.my_agent, -1, 1, 5).run())
         
-        # Obstacles on the left --> turn right
-        elif self.my_agent.i_state.obstacleInfo[0] == 1:
-            print("Obstacle on the left", self.my_agent.i_state.obstacleInfo)
-            self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent,
-                                                              direction=1,
-                                                              rotation_amount=15).run())
-        # Obstacles on the right --> turn left
-        elif self.my_agent.i_state.obstacleInfo[2] == 1:
-            print("Obstacle on the right", self.my_agent.i_state.obstacleInfo)
-            self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent,
-                                                             direction=-1,
-                                                             rotation_amount=15).run())
+#         # Obstacles on the left --> turn right
+#         elif self.my_agent.i_state.obstacleInfo[0] == 1:
+#             print("Obstacle on the left", self.my_agent.i_state.obstacleInfo)
+#             self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent,
+#                                                               direction=1,
+#                                                               rotation_amount=15).run())
+#         # Obstacles on the right --> turn left
+#         elif self.my_agent.i_state.obstacleInfo[2] == 1:
+#             print("Obstacle on the right", self.my_agent.i_state.obstacleInfo)
+#             self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent,
+#                                                              direction=-1,
+#                                                              rotation_amount=15).run())
         
-        elif self.my_agent.i_state.obstacleInfo[1] == 1:
-            print("Obstacle on the middle", self.my_agents.i_state.obstacleInfo)
-            self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent,
-                                                             direction=1,
-                                                             rotation_amount=15))
+#         elif self.my_agent.i_state.obstacleInfo[1] == 1:
+#             print("Obstacle on the middle", self.my_agents.i_state.obstacleInfo)
+#             self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent,
+#                                                              direction=1,
+#                                                              rotation_amount=15))
         
-    def update(self):
-        if not self.my_goal.done():
-            return pt.common.Status.RUNNING
-        else:
-            res = self.my_goal.result()
-            if res:
-                print("ManageObs completed with SUCCESS")
-                return pt.common.Status.SUCCESS
-            else:
-                print("ManageObs completed with FAILURE")
-                return pt.common.Status.FAILURE
+#     def update(self):
+#         if not self.my_goal.done():
+#             return pt.common.Status.RUNNING
+#         else:
+#             res = self.my_goal.result()
+#             if res:
+#                 print("ManageObs completed with SUCCESS")
+#                 return pt.common.Status.SUCCESS
+#             else:
+#                 print("ManageObs completed with FAILURE")
+#                 return pt.common.Status.FAILURE
 
-    def terminate(self, new_status: common.Status):
-        # Finishing the behaviour, therefore we have to stop the associated task
-        self.logger.debug("Terminate ManageObs")
-        self.my_goal.cancel()
+#     def terminate(self, new_status: common.Status):
+#         # Finishing the behaviour, therefore we have to stop the associated task
+#         self.logger.debug("Terminate ManageObs")
+#         self.my_goal.cancel()
 
 
+'''
+Makes the agent rotate the necessary amount of degrees to be directly looking at the detected flower
+'''
 class TurnToFlower(pt.behaviour.Behaviour):
     def __init__(self, aagent):
-        self.my_goal = None
-        print("Initializing turning to flower...")
         self.my_agent = aagent
+        self.my_goal = None
+        self.direction = None
+        self.degree_rotation = None        
+        print("Initializing TurnToFlower")        
         super(TurnToFlower, self).__init__("TurnToFlower")
-        self.logger.debug("TurnToFlower")
-        
+        self.logger.debug("TurnToFlower")        
     
     def initialise(self):
         self.direction = self.my_agent.i_state.flowerDirection[0]
-        self.degree_rotation = self.my_agent.i_state.flowerDirection[1]
-        direction = self.my_agent.i_state.flowerDirection
+        self.degree_rotation = self.my_agent.i_state.flowerDirection[1]        
         self.my_goal = asyncio.create_task(Turn(self.my_agent, self.direction, self.degree_rotation).run())
         
     def update(self):
-        if not self.my_goal.done():
-            # print("Still running...")
+        if not self.my_goal.done():            
             return pt.common.Status.RUNNING
         else:
             res = self.my_goal.result()
@@ -102,18 +104,22 @@ class TurnToFlower(pt.behaviour.Behaviour):
         self.my_goal.cancel()
 
 
-
+'''
+Executed after the TurnToFlower action. It moves forward until the distance that separates the
+agent from the detected flower is covered
+'''
 class GoToFlower(pt.behaviour.Behaviour):
     def __init__(self, aagent):
-        self.my_goal = None
-        print("Initializing going to the flower...")
         self.my_agent = aagent
+        self.my_goal = None
+        self.distance = None
+        print("Initializing GoToFlower")        
         super(GoToFlower, self).__init__("GoToFlower")
         self.logger.debug("GoToFlower")
     
     def initialise(self):
         self.distance = self.my_agent.i_state.flowerDistance
-        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, self.distance, -1, 1).run())
+        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, int(self.distance), -1, 1).run()) # Arrodonit
     
     def update(self):
         if not self.my_goal.done():
@@ -132,15 +138,17 @@ class GoToFlower(pt.behaviour.Behaviour):
         self.logger.debug("Terminate GoToFlower")
         self.my_goal.cancel()
 
+
+'''
+Executed after GoToFlower. The agent waits for five secons and is not hungry anymore
+'''
 class Eating(pt.behaviour.Behaviour):
     def __init__(self, aagent):
-        self.my_goal = None
-        print("Initializing eating...")
         self.my_agent = aagent
-        # self.i_state = aagent.i_state
-        super(Eating, self).__init__("Eating")
-        self.logger.debug("Eating")
         self.my_goal = None
+        print("Initializing Eating")        
+        super(Eating, self).__init__("Eating")
+        self.logger.debug("Eating")        
         
     def initialise(self):
         self.my_goal = asyncio.create_task(DoNothing(self.my_agent).run())
@@ -176,17 +184,21 @@ class Eating(pt.behaviour.Behaviour):
         self.my_goal.cancel()
 
 
+'''
+Makes the agent rotate the necessary amount of degrees to be directly looking at the detected astronaut
+'''
 class TurnToAstronaut(pt.behaviour.Behaviour):
     def __init__(self, aagent):
+        self.my_agent = aagent
         self.my_goal = None
+        self.direction = None
         print("Initializing TurnToAstronaut")
         super(TurnToAstronaut, self).__init__("TurnToAstronaut")
-        self.logger.debug("TurnToAstronaut")
-        self.my_agent = aagent
-
+        self.logger.debug("TurnToAstronaut")        
+                    # Different than the other turn?
     def initialise(self):
-        direction = self.my_agent.i_state.astronautDirection
-        self.my_goal = asyncio.create_task(Goals_BT.Turn(self.my_agent, *direction).run())
+        self.direction = self.my_agent.i_state.astronautDirection
+        self.my_goal = asyncio.create_task(Turn(self.my_agent, self.direction).run())
 
     def update(self):
         if not self.my_goal.done():
@@ -206,17 +218,22 @@ class TurnToAstronaut(pt.behaviour.Behaviour):
         self.my_goal.cancel()
 
 
+'''
+Executed after the TurnToAstronaut action. It moves forward until the distance that separates the
+agent from the detected astronaut is covered
+'''
 class GoToAstronaut(pt.behaviour.Behaviour):
     def __init__(self, aagent):
+        self.my_agent = aagent
         self.my_goal = None
+        self.distance = None
         print("Initializing GoToAstronaut")
         super(GoToAstronaut, self).__init__("GoToAstronaut")
-        self.logger.debug("GoToAstronaut")
-        self.my_agent = aagent
+        self.logger.debug("GoToAstronaut")        
 
     def initialise(self):
-        distance = self.my_agent.i_state.astronautDistance
-        self.my_goal = asyncio.create_task(Goals_BT.ForwardDist(self.my_agent, distance, -1, 1).run())
+        self.distance = self.my_agent.i_state.astronautDistance
+        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, self.distance, -1, 1).run())
 
     def update(self):
         if not self.my_goal.done():
@@ -235,7 +252,9 @@ class GoToAstronaut(pt.behaviour.Behaviour):
         self.logger.debug("Terminate GoToAstronaut")
         self.my_goal.cancel()
   
-        
+
+
+# Borrar?
 """"
 class Approach_Flower(pt.behaviour.Behaviour):
     STOPPED = 0
