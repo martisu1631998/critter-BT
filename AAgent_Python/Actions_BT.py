54810d6aba12e5faebcb12b5e2947fba451492a3
Arnau <1630311@uab.cat>
2024-04-20 13:39:12 +0200

    ordenant codi

--- AAgent_Python/Actions_BT.py
+++ AAgent_Python/Actions_BT.py
@@ -118,7 +118,7 @@ class GoToFlower(pt.behaviour.Behaviour):
     
     def initialise(self):
         self.distance = self.my_agent.i_state.flowerDistance
-        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, int(self.distance), -1, 1).run()) # Arrodonit
+        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, int(self.distance), -1, 1).run())
     
     def update(self):
         if not self.my_goal.done():
@@ -234,7 +234,7 @@ class GoToAstronaut(pt.behaviour.Behaviour):
 
     def initialise(self):
         self.distance = self.my_agent.i_state.astronautDistance
-        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, int(self.distance), -1, 1).run()) # Arrodonit
+        self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, int(self.distance), -1, 1).run())
 
     def update(self):
         if not self.my_goal.done():
@@ -352,117 +352,3 @@ class BN_Turn(pt.behaviour.Behaviour):
         # Finishing the behaviour, therefore we have to stop the associated task
         self.logger.debug("Terminate BN_Turn")
         self.my_goal.cancel()
-
-
-
-
-# Borrar?
-""""
-class Approach_Flower(pt.behaviour.Behaviour):
-    STOPPED = 0
-    TURNING = 1
-    MOVING_FORWARD = 2
-    
-    def __init__(self, aagent):
-        self.my_goal = None
-        print("Approaching flower...")
-        super(Approach_Flower, self).__init__("Approach_Flower")
-        self.my_agent = aagent
-        self.rays = 0
-        self.state = self.STOPPED
-        self.distance = None
-        self.i_state = aagent.i_state
-        
-    def initialise(self):
-        pass
-        # self.my_goal = asyncio.create_task(Goals_BT.ForwardDist(self.my_agent, -1, -1, 10).run())
-
-    def update(self):
-        left = [0,1,2,3]
-        middle = [4,5,6]
-        right = [7,8,9,10,11]
-        
-        if self.state == self.STOPPED:
-            print("Initializing things...")
-            self.direction = self.i_state.flowerDirection[0]
-            self.degree_rotation = self.i_state.flowerDirection[1]
-            self.distance = self.i_state.flowerDistance
-            self.state = self.TURNING
-        
-        elif self.state == self.TURNING:
-            BN_Turn(self.my_agent, self.direction, self.degree_rotation)
-            print("Successfully turned to the flower!")
-            self.state = self.MOVING_FORWARD
-            
-        elif self.state == self.MOVING_FORWARD:
-            BN_Forward(self.my_agent, self.distance)    
-            print("Approach_Flower SUCCESS")
-            return pt.common.Status.SUCCESS
-    
-
-    
-        if self.state == self.STOPPED:
-            sensor_obj_info = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
-            for index, value in enumerate(sensor_obj_info):
-                if value:
-                    if value["tag"] == "Flower":
-                        if index not in middle:
-                            self.rays = index
-                            self.state = self.TURNING
-        
-        elif self.state == self.TURNING:
-            if self.rays in left:
-                self.my_goal = asyncio.create_task(Turn(self.my_agent, direction=-1, rotation_amount=45).run())
-                print("Successfully turned to the left")
-                self.state = self.MOVING_FORWARD
-            
-            elif self.rays in right:
-                self.my_goal = asyncio.create_task(Turn(self.my_agent, direction=1, rotation_amount=45).run())
-                print("Successfully turned to the right")
-                self.state = self.MOVING_FORWARD
-                
-            sensor_obj_info = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
-            for index, value in enumerate(sensor_obj_info):
-                if value:
-                    if value["tag"] == "Flower":
-                        if index not in middle:
-                            self.distance = value["distance"]
-        
-        elif self.state == self.MOVING_FORWARD:
-            self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, self.distance, -1, 10).run())     
-            print("Approach_Flower SUCCESS")
-            return pt.common.Status.SUCCESS
-        
-        
-        sensor_obj_info = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
-        for index, value in enumerate(sensor_obj_info):
-            if value:
-                if value["tag"] == "Flower":
-                    print("Turning to the flower....")
-                    # Turn if the flower is not in front of the critter
-                    if index in left:
-                        self.my_goal = asyncio.create_task(Turn(self.my_agent, direction=1, rotation_amount=45).run())
-                        print("Successfully turned")
-                                
-                    if index in right:
-                        self.my_goal = asyncio.create_task(Turn(self.my_agent, direction=-1, rotation_amount=-45).run())
-                        print("Successfully turned")
-                    
-                    self.rays = index    
-                    
-                    break
-                          
-        # When the flower is in front of the critter, move towards it.
-        if self.rays in middle:
-            self.my_goal = asyncio.create_task(ForwardDist(self.my_agent, value["distance"], -1, 10).run())     
-            print("Approach_Flower SUCCESS")
-            return pt.common.Status.SUCCESS
-        
-        print("Approach_Flower completed with FAILURE")
-        return pt.common.Status.FAILURE
-
-    def terminate(self, new_status: common.Status):
-        pass
-        # self.logger.debug("Terminate BN_DetectFlower")
-        # self.my_goal.cancel()
-"""
